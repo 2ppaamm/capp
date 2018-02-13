7877ed91d6ad3cc97c21453cf36d0990c5df9e56
Pamela Lim <pamelaliusm@gmail.com>
2018-02-13 19:22:53 +0800

    fixed landing page with courses bottom

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../models/course';
 import { CourseService } from '../services/course.service';
+import { AuthService } from '../services/auth.service';
 declare var jQuery:any;
 declare var $ :any;
 
@@ -13,52 +14,50 @@ export class LandingComponent implements OnInit {
 
   courses: any;
   selectedCourse: Course;
-  constructor(private courseService: CourseService) { }
+  constructor(private courseService: CourseService, private authService:AuthService) { }
 
   ngOnInit() {
+	/* =================================
+	===  RESPONSIVE VIDEO           ====
+	=================================== */
 
-  /* =================================
-===  STELLAR                    ====
-=================================== */
-$(window).stellar({ 
-horizontalScrolling: false 
-});
+	$(".video-container").fitVids();
 
-    $(document).ready(function () {
+	    $(document).ready(function () {
 
-    $("#feedbacks").owlCarousel({
+	    $("#feedbacks").owlCarousel({
 
-        navigation: false, // Show next and prev buttons
-        slideSpeed: 800,
-        paginationSpeed: 400,
-        autoPlay: 5000,
-        singleItem: true
-    });
+	        navigation: false, // Show next and prev buttons
+	        slideSpeed: 800,
+	        paginationSpeed: 400,
+	        autoPlay: 5000,
+	        singleItem: true
+	    });
 
-    var owl = $("#screenshots");
+	    var owl = $("#screenshots");
 
-    owl.owlCarousel({
-        items: 4, //10 items above 1000px browser width
-        itemsDesktop: [1000, 4], //5 items between 1000px and 901px
-        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-        itemsTablet: [600, 1], //2 items between 600 and 0
-        itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
-    });
+	    owl.owlCarousel({
+	        items: 4, //10 items above 1000px browser width
+	        itemsDesktop: [1000, 4], //5 items between 1000px and 901px
+	        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+	        itemsTablet: [600, 1], //2 items between 600 and 0
+	        itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+	    });
 
 
-});
+	});
 
 
-/* =================================
-===  Nivo Lightbox              ====
-=================================== */
-$(document).ready(function () {
+	/* =================================
+	===  Nivo Lightbox              ====
+	=================================== */
+	$(document).ready(function () {
 
-    $('#screenshots a').nivoLightbox({
-        effect: 'fadeScale',
-    });
+	    $('#screenshots a').nivoLightbox({
+	        effect: 'fadeScale',
+	    });
 
-});
+	});
 
   	this.courses = this.courseService.getCourses();
     console.log(this.courses);
@@ -66,5 +65,9 @@ $(document).ready(function () {
 
   selectCourse(course: Course) {
   	this.selectedCourse = course;
+  }
+
+  public login(){
+  	this.authService.login();
   }
 }
