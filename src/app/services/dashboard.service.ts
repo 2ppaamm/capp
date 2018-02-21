57feb4beba0bcd5cad2f0629a3cb74fd5bcdcc3d
Pamela Lim <pamelaliusm@gmail.com>
2018-02-21 10:19:34 +0800

    enrolled classes done

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
-import { Dashboard } from '../models/dashboard';
 
 @Injectable()
 export class DashboardService {
